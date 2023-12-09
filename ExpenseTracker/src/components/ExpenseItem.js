d0ffe054a5e2856514bf362d392ef58869e727a2
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-12-09 14:15:30 +0530

    Updated.

--- ExpenseTracker/src/components/ExpenseItem.js
+++ ExpenseTracker/src/components/ExpenseItem.js
@@ -1,9 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const locationOfExpenditure = "Mumbai";
+
   return (
-    <div>
-      <h2>Food Rs 10</h2>
-      <h2>Petrol Rs 100</h2>
-      <h2>Movies Rs 200</h2>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{locationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
     </div>
   );
 }
