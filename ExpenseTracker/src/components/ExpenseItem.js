14238d51317a322f895bd7ea28aa115578b16a03
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-12-09 16:38:31 +0530

    Added components.

--- ExpenseTracker/src/components/ExpenseItem.js
+++ ExpenseTracker/src/components/ExpenseItem.js
@@ -1,4 +1,6 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
@@ -8,12 +10,8 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+      <ExpenseDate  date={props.date}/>
+      <ExpenseDetails amount={props.amount} location={props.location} title={props.title}/>
     </div>
   );
 }
