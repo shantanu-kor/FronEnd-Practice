57ac5c24bce29e8f0998cad9b4afd74820ada263
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-12-14 17:09:18 +0530

    added ReactDOM.

--- NameAge1/src/components/UI/ErrorModal.js
+++ NameAge1/src/components/UI/ErrorModal.js
@@ -5,22 +5,24 @@ import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
-const Backdrop = (props) => (
-  <div className={classes.backdrop} onClick={props.onConfirm}></div>
-);
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+};
 
 const ModalOverlay = (props) => {
-  <Card className={classes.modal}>
-    <header className={classes.header}>
-      <h2>{props.title}</h2>
-    </header>
-    <div className={classes.content}>
-      <p>{props.message}</p>
-    </div>
-    <footer className={classes.actions}>
-      <Button onClick={props.onConfirm}>Okay</Button>
-    </footer>
-  </Card>;
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
 };
 
 const ErrorModal = (props) => {
