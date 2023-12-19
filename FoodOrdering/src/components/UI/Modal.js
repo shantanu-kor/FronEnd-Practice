c355ac95df59176f2a53eaeabe6db5f64480f345
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-12-19 14:11:37 +0530

    close Cart.

--- FoodOrdering/src/components/UI/Modal.js
+++ FoodOrdering/src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,7 @@ const protalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <React.Fragment>
-      {ReactDOM.createPortal(<Backdrop />, protalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, protalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, protalElement)}
     </React.Fragment>
   );
