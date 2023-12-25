6cd7b5746ba4e13bc102a0b5b75614acef2fedff
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-12-25 21:13:06 +0530

    loading added.

--- MovieApp/src/App.js
+++ MovieApp/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const fetchData = await fetch("https://swapi.dev/api/films/");
     try {
       const response = fetchData;
@@ -28,6 +30,7 @@ function App() {
     } catch(e){
       console.log(e);
     }
+    setIsLoading(false);
   }
 
   return (
@@ -36,7 +39,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no Movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
