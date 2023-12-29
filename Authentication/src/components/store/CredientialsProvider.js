66e29e42e8d00c1f46d5cc909d17f83234ecad06
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-12-29 15:53:50 +0530

    added auto logout after 5 mins.

--- Authentication/src/components/store/CredientialsProvider.js
+++ Authentication/src/components/store/CredientialsProvider.js
@@ -3,26 +3,34 @@ import React, { useState } from "react";
 import CredientialsContext from "./credientialsContext";
 
 const CredientialsProvider = (props) => {
-    const [logStatus, setLogStatus] = useState(false);
-    
-    const removeIdTokenHandler = () => {
-        localStorage.removeItem("token");
-        setLogStatus(false);
-    };
+  const [logStatus, setLogStatus] = useState(false);
 
-    const addIdTokenHandler = (id) => {
-        localStorage.setItem("token", id);
-        setLogStatus(true);
-    };
-    
-    const credientials = {
-        idToken: localStorage.getItem("token"),
-        isLoggedIn: logStatus,
-        removeIdToken: removeIdTokenHandler,
-        addIdToken: addIdTokenHandler,
-    }
-    
-    return <CredientialsContext.Provider value={credientials}>{props.children}</CredientialsContext.Provider>
-}
+  const removeIdTokenHandler = () => {
+    localStorage.removeItem("token");
+    setLogStatus(false);
+  };
+
+  const addIdTokenHandler = (id) => {
+    localStorage.setItem("token", id);
+    setLogStatus(true);
+    setTimeout(() => {
+      localStorage.removeItem("token");
+      setLogStatus(false);
+    }, 300000);
+  };
+
+  const credientials = {
+    idToken: localStorage.getItem("token"),
+    isLoggedIn: logStatus,
+    removeIdToken: removeIdTokenHandler,
+    addIdToken: addIdTokenHandler,
+  };
+
+  return (
+    <CredientialsContext.Provider value={credientials}>
+      {props.children}
+    </CredientialsContext.Provider>
+  );
+};
 
 export default CredientialsProvider;
