faef6930840d87b83654d6045d64fa82cbaff90d
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2024-01-07 15:44:38 +0530

    added login and home page.

--- mailbox-Client/src/App.js
+++ mailbox-Client/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './App.css';
-import AuthPage from './pages/Authentication';
+import React, { useContext } from "react";
+import "./App.css";
+import AuthPage from "./pages/Authentication";
+import AuthContext from "./store/auth-context";
+import HomePage from "./pages/Home";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <React.Fragment>
-      <AuthPage />
+      {authCtx.idToken ? <HomePage /> : <AuthPage />}
     </React.Fragment>
   );
 }
--- mailbox-Client/src/pages/Authentication.js
+++ mailbox-Client/src/pages/Authentication.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SignUp from "../components/SignUp";
+import LogIn from "../components/LogIn";
+import { Button } from "react-bootstrap";
 
 const AuthPage = () => {
-  return <SignUp />;
+  const [login, setLogin] = useState(false);
+
+  const loginHandler = () => {
+    setLogin(true);
+  };
+
+  const signupHandler = () => {
+    setLogin(false);
+  };
+
+  return (
+    <React.Fragment>
+      {login ? (
+        <React.Fragment>
+          <LogIn />
+          <div className="my-2" style={{ textAlign: "center" }}>
+            <Button variant="dark" onClick={signupHandler}>
+              Don't have an account? Sign up
+            </Button>
+          </div>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <SignUp />
+          <div className="my-2" style={{ textAlign: "center" }}>
+            <Button variant="dark" onClick={loginHandler}>
+              Already an user? Login
+            </Button>
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default AuthPage;
